628f05f1be43b1b0ba43db0ef0fab3fe073c7b75
Dana Woodman <dana@danawoodman.com>
2015-12-12 13:40:49 -0800

    Add improved device list.

--- src/middlewares/template-locals.js
+++ src/middlewares/template-locals.js
@@ -2,6 +2,7 @@ export default () => {
   return (req, res, next) => {
     if (!res.locals) { res.locals = {} }
     res.locals.currentAccount = req.currentAccount
+    res.locals.moment = require('moment')
     next()
   }
 }
