3305af11415bec762968fe843c4b146ad685de11
Alex Wayne <alex@beautifulpixel.com>
2015-12-20 00:47:27 -0800

    derp

--- src/config/config.js
+++ src/config/config.js
@@ -22,7 +22,6 @@ export const MODELS_PATH = path.join(SOURCE_PATH, 'models')
 export const ASSETS_PATH = path.join(SOURCE_PATH, 'assets')
 export const PUBLIC_PATH = path.join(ROOT_PATH, 'public')
 export const INSTALLER_PATH = fs.readdirSync('./firmware')
-console.log(INSTALLER_PATH);
 
 // Cobot
 export const COBOT_SUBDOMAIN = env.COBOT_SUBDOMAIN
