642cd80fa478bde08b56c59b226bc36edfa89bd6
Dana Woodman <dana@danawoodman.com>
2016-04-05 13:29:44 -0700

    Fix card number.

--- src/routes/checkins/create.js
+++ src/routes/checkins/create.js
@@ -11,7 +11,9 @@ export default async (req, res) => {
   console.log('body', req.body)
   debug('checkin', req.body)
 
-  const accessToken = req.body.data
+  // Hack because cards start with three 0's but
+  // the Arduino app has them stripped off.
+  const accessToken = '000' + req.body.data
 
   const member = await Membership.findOne({ accessToken })
   console.log('member', member)
